1eab703256fc821620f04fe1c52616d7fb978275
Elizabeth Bartusiak <eeb50@duke.edu>
2018-11-06 15:45:49 -0500

    mainactivity buttons for lost/found reports

--- app/src/main/java/edu/duke/compsci316/lostitfoundit/MainActivity.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.duke.compsci316.lostitfoundit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonLost = findViewById(R.id.button_lost);
+        buttonLost.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, EnterReportActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        final Button buttonFound = findViewById(R.id.button_found);
+        buttonFound.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, EnterReportActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+
+
+
     }
 }
