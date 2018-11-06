e3dcb0521bbc1e010b3555fb7145f09275772608
Ryan Hill <rdh35@duke.edu>
2018-11-06 15:53:53 -0500

    Updated FoundReport.java and Item.java

--- app/src/main/java/edu/duke/compsci316/lostitfoundit/Item.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/Item.java
@@ -5,4 +5,35 @@ package edu.duke.compsci316.lostitfoundit;
  */
 
 public class Item {
+    private String myName;//name of item
+    private String myType;//item's type
+    private String myDesc;//user's description of item
+
+    /*Constructor*/
+    public Item(String name, String type, String desc) {
+        this.myName = name;
+        this.myType = type;
+        this.myDesc = desc;
+    }
+
+    /**             Accessors               **/
+    public String getName() {
+        return this.myName;
+    }
+    public String getType() {
+        return this.myType;
+    }
+    public String getDescription() {
+        return this.myDesc;
+    }
+    /**             Mutators                **/
+    public void setName(String n) {
+        this.myName = n;
+    }
+    public void setType(String t) {
+        this.myType = t;
+    }
+    public void setDescription(String d) {
+        this.myDesc = d;
+    }
 }
