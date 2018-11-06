0380f6564885c18ce203c4026fdcb4e1cf92c34a
Elizabeth Bartusiak <eeb50@duke.edu>
2018-11-06 15:57:12 -0500

    start queryResultsActivity

--- app/src/main/java/edu/duke/compsci316/lostitfoundit/QueryResultsActivity.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/QueryResultsActivity.java
@@ -3,6 +3,12 @@ package edu.duke.compsci316.lostitfoundit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Shows results for queries
+ * @author Elizabeth Bartusiak
+ * @version 11/06/2018
+ */
+
 public class QueryResultsActivity extends AppCompatActivity {
 
     @Override
