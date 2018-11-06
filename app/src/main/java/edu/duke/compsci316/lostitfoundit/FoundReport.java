281130eb5ac57e0512118469a521e8622aa3b6b0
Ryan Hill <rdh35@duke.edu>
2018-11-06 15:42:07 -0500

    Basic definition of FoundReport and its construction

--- app/src/main/java/edu/duke/compsci316/lostitfoundit/FoundReport.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/FoundReport.java
@@ -5,4 +5,61 @@ package edu.duke.compsci316.lostitfoundit;
  */
 
 public class FoundReport {
+    /*FoundReport attributes*/
+    private String myName;//name of item
+    private String myType;//item's type
+    private String myDesc;//user's description of item
+    private String myTime;//report's timestamp
+    private String myLocation;//location found
+    private String myLeftLoc;//location where item was left/can be retrieved
+
+    /*Constructor*/
+    public FoundReport(String name, String type, String desc, String time, String loc, String leftLoc) {
+        this.myName = name;
+        this.myType = type;
+        this.myDesc = desc;
+        this.myTime = time;
+        this.myLocation = loc;
+        this.myLeftLoc = leftLoc;
+    }
+
+    /**                 Accessors               **/
+    public String getName() {
+        return this.myName;
+    }
+    public String getType() {
+        return this.myType;
+    }
+    public String getDescription() {
+        return this.myDesc;
+    }
+    public String getTime() {
+        return this.myTime;
+    }
+    public String getFoundLocation() {
+        return this.myLocation;
+    }
+    public String getLeftLocation() {
+        return this.myLeftLoc;
+    }
+
+    /**              Mutators                   **/
+    public void setName(String n) {
+        this.myName = n;
+    }
+    public void setType(String t) {
+        this.myType = t;
+    }
+    public void setDescription(String d) {
+        this.myDesc = d;
+    }
+    public void setTime(String t) {
+        this.myTime = t;
+    }
+    public void setLocation(String l) {
+        this.myLocation = l;
+    }
+    public void setLeftLocation(String l) {
+        this.myLeftLoc = l;
+    }
 }
