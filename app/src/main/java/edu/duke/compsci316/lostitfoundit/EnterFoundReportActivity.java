537cd0f4eb67ba79d8635f0f9d61193989746591
Elizabeth Bartusiak <eeb50@duke.edu>
2018-11-15 22:45:32 -0500

    required fields and dropdown menu outlines

--- app/src/main/java/edu/duke/compsci316/lostitfoundit/EnterFoundReportActivity.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/EnterFoundReportActivity.java
@@ -1,7 +1,15 @@
 package edu.duke.compsci316.lostitfoundit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class EnterFoundReportActivity extends AppCompatActivity {
 
@@ -9,5 +17,58 @@ public class EnterFoundReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_found_report);
+
+        final EditText itemTitle = findViewById(R.id.item_title_editText);
+
+        /* following code from
+            https://stackoverflow.com/questions/13377361/how-to-create-a-drop-down-list
+         */
+        //get the spinner from the xml.
+        final Spinner dropdown = findViewById(R.id.type_spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"What TYPE of item is it?", "water bottle", "clothing", "electronic", "DukeID"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        //LOCATION SPINNER
+        final Spinner dropdownLocation = findViewById(R.id.location_spinner);
+        //create a list of items for the spinner.
+        String[] places = new String[]{"Where did you find it?", "West Union", "Bryan Center", "Divinity School", "East Campus"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> locationAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, places);
+        //set the spinners adapter to the previously created one.
+        dropdownLocation.setAdapter(locationAdapter);
+
+        final Button button = findViewById(R.id.found_submit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(itemTitle.getText().toString().equals("")){
+                    Toast.makeText(EnterFoundReportActivity.this, "Item Title required",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if(String.valueOf(dropdown.getSelectedItem()).equals("What TYPE of item is it?")){
+                    Toast.makeText(EnterFoundReportActivity.this, "TYPE field required",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if(String.valueOf(dropdownLocation.getSelectedItem()).equals("Where did you find it?")){
+                    Toast.makeText(EnterFoundReportActivity.this, "Location field required",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(EnterFoundReportActivity.this, "Successfully submitted",
+                            Toast.LENGTH_LONG).show();
+                    Intent myIntent = new Intent(EnterFoundReportActivity.this, MainActivity.class);
+                    startActivity(myIntent);
+                }
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/edu/duke/compsci316/lostitfoundit/EnterLostReportActivity.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/EnterLostReportActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class EnterLostReportActivity extends AppCompatActivity {
 
@@ -13,11 +17,55 @@ public class EnterLostReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_lost_report);
 
+        final EditText itemTitle = findViewById(R.id.lost_report_editText);
+
+        /* following code from
+            https://stackoverflow.com/questions/13377361/how-to-create-a-drop-down-list
+         */
+        //get the spinner from the xml.
+        final Spinner dropdown = findViewById(R.id.lost_report_type_spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"What TYPE of item is it?", "water bottle", "clothing", "electronic", "DukeID"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        //LOCATION SPINNER
+        final Spinner dropdownLocation = findViewById(R.id.lost_report_location_spinner);
+        //create a list of items for the spinner.
+        String[] places = new String[]{"Where did you find it?", "West Union", "Bryan Center", "Divinity School", "East Campus"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> locationAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, places);
+        //set the spinners adapter to the previously created one.
+        dropdownLocation.setAdapter(locationAdapter);
+
         final Button button = findViewById(R.id.lost_report_submit_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent myIntent = new Intent(EnterLostReportActivity.this, QueryResultsActivity.class);
-                startActivity(myIntent);
+                if(itemTitle.getText().toString().equals("")){
+                    Toast.makeText(EnterLostReportActivity.this, "Item Title required",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if(String.valueOf(dropdown.getSelectedItem()).equals("What TYPE of item is it?")){
+                    Toast.makeText(EnterLostReportActivity.this, "TYPE field required",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if(String.valueOf(dropdownLocation.getSelectedItem()).equals("Where did you find it?")){
+                    Toast.makeText(EnterLostReportActivity.this, "Location field required",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(EnterLostReportActivity.this, "Successfully submitted",
+                            Toast.LENGTH_LONG).show();
+                    Intent myIntent = new Intent(EnterLostReportActivity.this, MainActivity.class);
+                    startActivity(myIntent);
+                }
+
+
             }
         });
     }
