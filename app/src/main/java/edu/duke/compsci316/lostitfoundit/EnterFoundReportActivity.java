124211c885705bb20fe79941f3501b9f58df7f47
Ryan Hill <rdh35@duke.edu>
2018-11-27 19:30:36 -0500

    Updated with basis for camera usage

--- app/src/main/java/edu/duke/compsci316/lostitfoundit/EnterFoundReportActivity.java
+++ app/src/main/java/edu/duke/compsci316/lostitfoundit/EnterFoundReportActivity.java
@@ -44,6 +44,28 @@ public class EnterFoundReportActivity extends AppCompatActivity {
         //set the spinners adapter to the previously created one.
         dropdownLocation.setAdapter(locationAdapter);
 
+        //camera part
+        static final int REQUEST_IMAGE_CAPTURE = 1;
+
+        //insert new button
+        final Button imgButton = findViewById(R.id.img_button);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            private void dispatchTakePictureIntent() {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+            @Override
+            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    mImageView.setImageBitmap(imageBitmap);
+                }
+            }
+        });
+
         final Button button = findViewById(R.id.found_submit_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
